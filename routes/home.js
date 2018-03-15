a4dd7cb49f7fbf4ca2d63491c144f03fc5eeb7aa
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-03-14 23:55:31 -0700

    Adding new API entry docs to Home

--- routes/home.js
+++ routes/home.js
@@ -6,22 +6,38 @@ router.get('/', function(req, res, next) {
   res.send({
     message: 'EndPoints for API',
     posts: {
-      root: 'GET /posts',
+      root: '/posts',
       all: 'GET /posts',
       id: 'GET /posts/{id}',
       add: 'POST /posts'
     },
     users: {
-      root: 'GET /users'
+      root: '/users'
     },
     carousels: {
-      root: 'GET /carousels',
+      root: '/carousels',
       all: 'GET /carousels',
       id: 'GET /carousels/{id}',
       add: 'POST /carousels',
       update: 'PUT /carousels/{id}',
       delete: 'DELETE /carousels/{id}'
 
+    },
+    occupations: {
+      root: '/occupations',
+      all: 'GET /occupations',
+      id: 'GET /occupations/{id}',
+      add: 'POST /occupations',
+      update: 'PUT /occupations/{id}',
+      delete: 'DELETE /occupations/{id}'
+    },
+    professionals: {
+      root: '/occupations/{occupationId}/professionals',
+      all: 'GET /occupations/{occupationId}/professionals',
+      id: 'GET /occupations/{occupationId}/professionals/{professionalId}',
+      add: 'POST /occupations/{occupationId}/professionals',
+      update: 'PUT /occupations/{occupationId}/professionals',
+      delete: 'DELETE /occupations/{occupationId}/professionals'
     }
   });
 });
